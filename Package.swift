f0e5511f832c7edfef7aaf6d30eadc43f9bfc903
Roman Suvorov <roman.suvorov@onseo.biz>
2024-12-26 17:00:26 +0200

    Bump to iOS 12

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
   name: "Lottie",
   // Minimum platform versions should be kept in sync with the per-platform targets in Lottie.xcodeproj, lottie-ios.podspec, and lottie-spm's Package.swift
-  platforms: [.iOS("11.0"), .macOS("10.11"), .tvOS("11.0"), .custom("visionOS", versionString: "1.0")],
+  platforms: [.iOS("12.0"), .macOS("10.11"), .tvOS("11.0"), .custom("visionOS", versionString: "1.0")],
   products: [
     .library(name: "Lottie", targets: ["Lottie"]),
     // Product for dynamic linking, as SPM builds libraries statically by default.
